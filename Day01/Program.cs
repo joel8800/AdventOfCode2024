ac237ee2a47c81d4e99ed7871ca42049e559c5cd
Joel Kaneshiro <v-jkaneshiro@microsoft.com>
2024-12-02 17:50:15 -0600

    Code cleanup on Day01

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,5 +1,4 @@
-﻿
-Console.WriteLine("Day 01: Historian Hysteria");
+﻿Console.WriteLine("Day 01: Historian Hysteria");
 
 string[] input = File.ReadAllLines("input.txt");
 
@@ -9,9 +8,9 @@ List<int> right = [];
 
 foreach (string line in input)
 {
-    int[] numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-    left.Add(numbers[0]);
-    right.Add(numbers[1]);
+    string[] nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    left.Add(int.Parse(nums[0]));
+    right.Add(int.Parse(nums[1]));
 }
 
 left.Sort();
@@ -25,13 +24,9 @@ for (int i = 0; i < left.Count; i++)
 int scorePt2 = 0;
 
 for (int i = 0; i < left.Count; i++)
-{
-    int appearances = right.Where(x => x == left[i]).Count();
+    scorePt2 += left[i] * right.Where(x => x == left[i]).Count();
 
-    scorePt2 += left[i] * appearances;
-}
-
-Console.WriteLine($"Part 1: {totalPt1}"); // 2756096
-Console.WriteLine($"Part 2: {scorePt2}"); // 23117829
+Console.WriteLine($"Part 1: {totalPt1}");
+Console.WriteLine($"Part 2: {scorePt2}");
 
 // ============================================================================
