2810082ed356536215da24edc152e1c0f3ae32a7
Joel Kaneshiro <joel8800@gmail.com>
2025-01-02 19:41:54 -0800

    Solve Day14

---
+++ Day14/Program.cs
@@ -0,0 +1,104 @@
+﻿using Day14;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Day 14: Restroom Redoubt");
+
+string[] input = File.ReadAllLines("input.txt");
+
+// grid size
+int maxX = 101;   // sample 11, input 101
+int maxY = 103;   // sample 7 , input 103
+
+List<Robot> robotsPt1 = [];
+List<Robot> robotsPt2 = [];
+Robot.SetMax(maxX, maxY);
+
+foreach (string line in input)
+{
+    MatchCollection mc = Regex.Matches(line, @"\-?\d+");
+    int px = int.Parse(mc[0].Value);
+    int py = int.Parse(mc[1].Value);
+    int vx = int.Parse(mc[2].Value);
+    int vy = int.Parse(mc[3].Value);
+
+    Robot bot1 = new(px, py, vx, vy);
+    Robot bot2 = new(px, py, vx, vy);
+    robotsPt1.Add(bot1);
+    robotsPt2.Add(bot2);
+}
+
+int[] quad = [0, 0, 0, 0, 0];   // [0] is for bots on center axes
+
+foreach (Robot r in robotsPt1)
+{
+    r.MoveN(100);
+    quad[r.GetQuadrant()]++;
+}
+
+//Console.WriteLine($"Q1:{quad[1]} Q2:{quad[2]} Q3:{quad[3]} Q4:{quad[4]}               ");
+//Console.WriteLine($"part1 answer: {quad[1] * quad[2] * quad[3] * quad[4]}");
+int answerPt1 = quad[1] * quad[2] * quad[3] * quad[4];
+
+// ----------------------------------------------------------------------------
+
+int answerPt2 = 0;
+
+while (true)
+{
+    answerPt2++;
+    quad = [0, 0, 0, 0, 0];
+    foreach (Robot r in robotsPt2)
+    {
+        r.Move();
+        quad[r.GetQuadrant()]++;
+    }
+
+    if (answerPt2 > 6350 || answerPt2 == 1)
+    {
+        //PrintGrid(robotsPt2, answerPt2);
+        //Console.WriteLine($"Q1:{quad[1]} Q2:{quad[2]} Q3:{quad[3]} Q4:{quad[4]}               ");
+        //Console.WriteLine($"quads multiplied: {quad[1] * quad[2] * quad[3] * quad[4]}");
+
+        if (quad[4] > 300)  // majority of robots are in quad 4
+            break;
+    }
+
+    if (answerPt2 >= 6400)
+        break;
+}
+
+Console.WriteLine($"Part 1: {answerPt1}");
+Console.WriteLine($"Part 2: {answerPt2}");
+
+// ============================================================================
+
+void PrintGrid(List<Robot> robots, int iteration)
+{
+    if (iteration == 1)
+        Console.Clear();
+
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"Iteration: {iteration}        ");
+
+    char[,] grid = new char[maxY, maxX];
+    for (int y = 0; y < maxY; y++)
+    {
+        for (int x = 0; x < maxX; x++)
+        {
+            grid[y, x] = '.';
+        }
+    }
+    foreach (Robot r in robots)
+    {
+        grid[r.PosY, r.PosX] = '#';
+    }
+    for (int y = 0; y < maxY; y++)
+    {
+        for (int x = 0; x < maxX; x++)
+        {
+            Console.Write(grid[y, x]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}   
---
+++ Day14/Robot.cs
@@ -0,0 +1,45 @@
+﻿namespace Day14
+{
+    internal class Robot(int posX, int posY, int velX, int velY)
+    {
+        private static int maxX;
+        private static int maxY;
+
+        public int PosX = posX;
+        public int PosY = posY;
+        private readonly int Vx = velX;
+        private readonly int Vy = velY;
+
+        static public void SetMax(int x, int y)
+        {
+            maxX = x;
+            maxY = y;
+        }
+
+        public void Move()
+        {
+            PosX = (PosX + Vx + maxX) % maxX;
+            PosY = (PosY + Vy + maxY) % maxY;
+        }
+
+        public void MoveN(int n)
+        {
+            PosX = (PosX + (Vx + maxX) * n) % maxX;
+            PosY = (PosY + (Vy + maxY) * n) % maxY;
+        }
+
+        // return quadrant number (1-4) or 0 if on middle row or column
+        public int GetQuadrant()
+        {
+            if (PosX < maxX / 2 && PosY < maxY / 2)
+                return 1;
+            if (PosX > maxX / 2 && PosY < maxY / 2)
+                return 2;
+            if (PosX < maxX / 2 && PosY > maxY / 2)
+                return 3;
+            if (PosX > maxX / 2 && PosY > maxY / 2)
+                return 4;
+            return 0;
+        }
+    }
+}
